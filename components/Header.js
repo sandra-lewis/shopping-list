4db9216593134fa36af153b071e26d41668d2e4f
sandra-lewis <sandra.lewis279@gmail.com>
2020-08-02 19:57:12 +0530

    First Commit

---
+++ components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
+
+const Header = ({title}) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: 'darkslateblue',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 23,
+    textAlign: 'center',
+  },
+});
+
+export default Header;
